ac3cff520ded12e0720537eec29529a014719e38
akak01 <syrgakovaakak010903@gmail.com>
2021-04-15 14:53:18 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route } from "react-router";
+import { Link } from "react-router-dom";
+import About from "./components/About/A";
+import Contacts from "./components/Contacts/Contacts";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        <li>
+        <NavLink activeClassName="active" to="/" exact>Home</NavLink>
+        </li>  
+        <li>
+        <NavLink activeClassName="active" to="/about" exact>About</NavLink>
+        </li> 
+        <li>
+        <NavLink activeClassName="active" to="/contacts" exact>Contacts</NavLink>
+        </li> 
+      </ul>     
+
+      <Switch>
+        <Route path="/" component={Home} exact />
+        <Route path="/about" component={About} />
+        <Route path="/contacts" component={Contacts}  />
+        <Route path="/" render={() => <h1>Page not found</h1>} />
+        {/* <Redirect to="/" /> */}
+      </Switch>  
     </div>
   );
 }
